77026ad4bd977e033c62c27cc8614489541eefa7
R-unic <roleyspersonal@gmail.com>
2024-12-21 02:20:50 -0500

    feat: show inputs for theories

--- src/test-runner.ts
+++ src/test-runner.ts
@@ -4,26 +4,36 @@ import { StringBuilder } from "@rbxts/string-builder";
 import { getDescendantsOfType } from "@rbxts/instance-utility";
 import { flatten, reverse } from "@rbxts/array-utils";
 import Object from "@rbxts/object-utils";
-import repr from "@rbxts/repr";
 
 import { type Maybe, Meta } from "./common";
+import repr from "@rbxts/repr";
 
-const GREEN = "\x1b[32m";
-const RED = "\x1b[31m";
-const RESET = "\x1b[0m";
+type TestClassInstance = Record<string, Callback>;
 
 interface TestCaseResult {
 	readonly errorMessage?: string;
 	readonly timeElapsed: number;
+	readonly inputs?: unknown[];
 }
 
-type TestClassInstance = Record<string, Callback>;
+interface TestRunOptions {
+	readonly reporter: (testResults: string) => void;
+	readonly colors: boolean;
+}
+
+const GREEN = "\x1b[32m";
+const RED = "\x1b[31m";
+const RESET = "\x1b[0m";
+const DEFAULT_TEST_RUN_OPTIONS: TestRunOptions = {
+	reporter: print,
+	colors: false
+};
 
 class TestRunner {
 	private readonly testClasses: [Constructor<TestClassInstance>, TestClassInstance][];
+	private results = new Map<Constructor, Record<string, TestCaseResult>>;
 	private failedTests = 0;
 	private passedTests = 0;
-	private results = new Map<Constructor, Record<string, TestCaseResult>>;
 
 	public constructor(...roots: Instance[]) {
 		this.testClasses = flatten(roots.map(root => getDescendantsOfType(root, "ModuleScript")))
@@ -35,48 +45,49 @@ class TestRunner {
 			});
 	}
 
-	public async run(reporter: (testResults: string) => void = print, colors = false): Promise<void> {
+	public async run(options: Partial<TestRunOptions> = DEFAULT_TEST_RUN_OPTIONS): Promise<void> {
 		this.results = new Map;
 
+		const { reporter, colors }: TestRunOptions = Object.assign({}, DEFAULT_TEST_RUN_OPTIONS, options);
 		const start = os.clock();
 		for (const [TestClass, testClass] of this.testClasses) {
 			const properties = Reflect.getProperties(TestClass);
 			const factNames = properties.filter(property => Reflect.hasMetadata(TestClass, Meta.Fact, property))
 			const theoryNames = properties.filter(property => Reflect.hasMetadata(TestClass, Meta.Theory, property));
 
-			const fail = (exception: unknown, name: string, timeElapsed: number, extra?: string): void => {
+			const fail = (exception: unknown, name: string, { timeElapsed, inputs }: Omit<TestCaseResult, "errorMessage">): void => {
 				this.failedTests++;
 
 				let classResults = this.results.get(TestClass);
 				if (classResults === undefined)
 					classResults = this.results.set(TestClass, {}).get(TestClass)!;
 
-				classResults[`${name}${extra !== undefined ? " - " + extra : ""}`] = {
+				classResults[name] = {
 					errorMessage: tostring(exception),
-					timeElapsed
+					timeElapsed,
+					inputs
 				};
 			}
-			const pass = (name: string, timeElapsed: number, extra?: string): void => {
+			const pass = (name: string, { timeElapsed, inputs }: Omit<TestCaseResult, "errorMessage">): void => {
 				this.passedTests++;
 				let classResults = this.results.get(TestClass);
 				if (classResults === undefined)
 					classResults = this.results.set(TestClass, {}).get(TestClass)!;
 
-				classResults[`${name}${extra !== undefined ? " | " + extra : ""}`] = { timeElapsed };
+				classResults[name] = { timeElapsed, inputs };
 			}
 			const runTestCase = async (testCase: Callback, name: string, args?: unknown[]): Promise<boolean> => {
-				const inputDisplay = args !== undefined ? `input: (${(<defined[]>args).map(v => repr(v, { pretty: false })).join(", ")})` : undefined;
 				const start = os.clock();
 				try {
 					await testCase(testClass, ...args ?? []);
 				} catch (e) {
 					const timeElapsed = os.clock() - start;
-					fail(e, name, timeElapsed, inputDisplay);
+					fail(e, name, { timeElapsed, inputs: args });
 					return false;
 				}
 
 				const timeElapsed = os.clock() - start;
-				pass(name, timeElapsed, inputDisplay);
+				pass(name, { timeElapsed, inputs: args });
 				return true;
 			};
 
@@ -118,23 +129,40 @@ class TestRunner {
 				.map(([_, { timeElapsed }]) => timeElapsed)
 				.reduce((sum, n) => sum + n);
 
+			let lastTestCaseName = testCases[0][0];
 			results.appendLine(`[${getSymbol(allPassed)}] ${TestClass} (${math.round(totalTime * 1000)}ms)`);
 			indent++;
+
 			for (const testCase of testCases) {
-				const [testCaseName, { errorMessage, timeElapsed }] = testCase;
+				const [testCaseName, { errorMessage, timeElapsed, inputs }] = testCase;
+				const totalElapsed = testCases
+					.filter(([name]) => name === testCaseName)
+					.map(([_, { timeElapsed }]) => timeElapsed)
+					.reduce((sum, n) => sum + n);
+
 				const isLast = testCases.indexOf(testCase) === testCases.size() - 1;
 				const passed = errorMessage === undefined;
 				appendIndent();
-				results.appendLine(`${isLast ? "└" : "├"}── [${getSymbol(passed)}] ${testCaseName} (${math.round(timeElapsed * 1000)}ms)`)
+
+				if (testCaseName !== lastTestCaseName)
+					results.appendLine(`${isLast ? "└" : "├"}── [${getSymbol(passed)}] ${testCaseName} (${math.round(totalElapsed * 1000)}ms)`);
+				else {
+					appendIndent();
+					results.appendLine(`${isLast ? "└" : "├"}── [${getSymbol(passed)}] (${this.formatInputs(inputs)}) (${math.round(timeElapsed * 1000)}ms)`);
+				}
+
+				lastTestCaseName = testCaseName;
 			}
+
 			indent--;
 		}
 
 		results.appendLine("");
 		for (const [_, testCases] of pairs(this.results))
-			for (const [testCaseName, { errorMessage }] of pairs(testCases)) {
+			for (const [testCaseName, { errorMessage, inputs }] of pairs(testCases)) {
 				if (errorMessage === undefined) continue;
-				results.append(testCaseName + " - ");
+				results.appendLine(testCaseName);
+				results.appendLine(`Inputs: ${this.formatInputs(inputs)}`);
 				results.appendLine(tostring(errorMessage));
 				results.appendLine("");
 			}
@@ -145,6 +173,12 @@ class TestRunner {
 		results.appendLine(`${colors ? RED : ""}Failed: ${this.failedTests}${colors ? RESET : ""}`);
 
 		return results.toString();
+	}
+
+	private formatInputs(inputs: unknown[] | undefined) {
+		return inputs !== undefined ?
+			`(${(<defined[]>inputs).map(v => repr(v, { pretty: false })).join(", ")})`
+			: "";
 	}
 }
 
